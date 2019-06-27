7fa26d120012b2d5145bd72e4d0c314bc1cef034
Grzegorz Kułaga <poczta@grzegorzkulaga.pl>
2019-06-27 11:18:27 +0200

    Najmniejsza średnia kolumny

--- Tablica2d/Tablica2d/Program.cs
+++ Tablica2d/Tablica2d/Program.cs
@@ -10,6 +10,68 @@ namespace Tablica2d
     {
         static void Main(string[] args)
         {
+            int size;
+            Random losowa = new Random();
+
+            Console.Write("Wprowadź rozmiar tablicy: ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            int[,] tablica = new int[size, size];
+
+            for(int i=0; i < size; i++)
+            {
+                for(int j=0; j<size; j++)
+                {
+                    tablica[i, j] = losowa.Next(1, 10);
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("\n");
+                for(int j=0; j<size; j++)
+                {
+                    Console.Write(" " + tablica[i, j]);
+                }
+            }
+            Console.WriteLine();
+
+            int srednia = 0;
+            int[] suma = new int[size];
+
+            for(int i=0; i < size; i++)
+            {
+
+                for (int j=0; j<size; j++)
+                {
+                    suma[j] = suma[j] + tablica[i, j];
+                }
+            }
+
+            Console.Write("\nSuma: ");
+            for(int j=0; j<size; j++)
+            {
+                Console.Write(suma[j] + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Średnia: ");
+            for (int j=0; j<size; j++)
+            {
+                suma[j] = suma[j] / size;
+                Console.Write(suma[j] + " ");
+            }
+            Console.WriteLine();
+
+            srednia = suma[0];
+            for (int j=0; j<size; j++)
+            {
+                if(suma[j]<srednia)
+                {
+                    srednia = suma[j];
+                }
+            }
+            Console.WriteLine("Najmniejsza średnia: " + srednia);
         }
     }
 }
